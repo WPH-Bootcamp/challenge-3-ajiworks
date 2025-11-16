ca5ad4dd62e9e44e3a950a6f0b045139a67c9df6
ajiworks <sntso.aji@gmail.com>
2025-11-16 23:30:19 +0700

    maaf belum selesai

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // code here, goodluck!!
+'use-strict';
+const prompt = require('prompt-sync')();
+
+// Tugas 1. User Input Handling
+function getValidNumberInput(promptMessage) {
+    let input = prompt(promptMessage);
+    let number = Number(input);
+
+    while (isNaN(number)) {
+        console.log("input tidak valid. Harus berupa angka!");
+        input = prompt(promptMessage);
+        number = Number(input);
+    }
+
+    return number;
+}
+
+
+
+
+
+function getValidOperatorInput(promptMessage) {
+    const validOperators = ["+", "-", "*", "/", "%", "**"];
+    let operator = prompt(promptMessage);
+
+    while (!validOperators.includes(operator)) {
+        console.log("Operator tidak valid. Pilih salah satu: + - * / % **")
+        operator = prompt(promptMessage);
+    }
+
+    return operator;
+}
+
+
+
+
+
